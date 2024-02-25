15cac291b7b3e82a897722e722a5f575ed04026a
Adnan <adakh3@gmail.com>
2024-02-25 10:28:10 +0000

    updated db in settings

--- fin_analysis_django/settings.py
+++ fin_analysis_django/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'fin_analysis_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydatabase.db',  # Ensure this is a string
     }
 }
+
+
 
 
 # Password validation
